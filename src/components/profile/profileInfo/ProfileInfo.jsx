b161bceb944e1af5370cab4677b7d182fe744321
Vika <kurilova2003@gmail.com>
2023-07-20 14:55:39 +0300

    add: information about users profile

--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -1,12 +1,18 @@
 import s from "./ProfileInfo.module.css"
-export const ProfileInfo = () => {
+import Preloader from "../../../common/preloader/Preloader";
+export const ProfileInfo = (props) => {
+	if (!props.profile) {
+		return <Preloader />
+	}
 	return (
 		<div>
 			<div>
 				<img src="https://wallpaperaccess.com/full/707529.jpg" className={s.profileBackground} alt="Content" />
 			</div>
 			<div className={s.descriptionBlock}>
-				ava+disc
+				<img src={props.profile.photos.large} alt={'Content'}/>
+				<div>{props.profile.fullName}</div>
+				<div>{props.profile.aboutMe}</div>
 			</div>
 		</div>
 	)
--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -1,11 +1,13 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+const SET_USER_PROFILE = 'SET-USER-PROFILE'
 let initialState = {
     posts: [
             {id: 1, message: 'Hi, how are you?', likesCount: 12},
             {id: 2, message: 'It is my first post!', likesCount: 26},
         ],
-    newPostText: 'vvvkks.com'
+    newPostText: 'vvvkks.com',
+    profile: null
 };
 const profileReducer = (state = initialState, action) => {
     switch (action.type) {
@@ -25,6 +27,11 @@ const profileReducer = (state = initialState, action) => {
                 ...state,
                 newPostText: action.newText
             }
+        case SET_USER_PROFILE:
+            return {
+                ...state,
+                profile: action.profile
+            }
         default:
             return state;
     }
@@ -34,5 +41,8 @@ export const addPostActionCreator = () => ({
 })
 export const updateNewPostTextActionCreator = (text) => ({
     type: UPDATE_NEW_POST_TEXT, newText: text
+})
+export const setUserProfile = (profile) => ({
+    type: SET_USER_PROFILE, profile
 })
 export default profileReducer;
