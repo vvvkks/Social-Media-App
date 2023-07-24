57b113c2e445be7dbcb774fce3a11f909abd7db9
Vika <kurilova2003@gmail.com>
2023-07-24 16:08:39 +0300

    add: Status for users profiles

--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -1,18 +1,20 @@
 import s from "./ProfileInfo.module.css"
 import Preloader from "../../../common/preloader/Preloader";
+import ProfileStatus from "./ProfileStatus";
 export const ProfileInfo = (props) => {
 	if (!props.profile) {
 		return <Preloader />
 	}
 	return (
 		<div>
-			<div>
-				<img src="https://wallpaperaccess.com/full/707529.jpg" className={s.profileBackground} alt="Content" />
-			</div>
+			{/*<div>*/}
+			{/*	<img src="https://wallpaperaccess.com/full/707529.jpg" className={s.profileBackground} alt="Content" />*/}
+			{/*</div>*/}
 			<div className={s.descriptionBlock}>
 				<img src={props.profile.photos.large} alt={'Content'}/>
 				<div>{props.profile.fullName}</div>
 				<div>{props.profile.aboutMe}</div>
+				<ProfileStatus status={"hello"}/>
 			</div>
 		</div>
 	)
