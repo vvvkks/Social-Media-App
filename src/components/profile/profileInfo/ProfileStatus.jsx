9f80057a431b5f7fc8508f38b5b09ae76259a86d
Vika <kurilova2003@gmail.com>
2023-07-30 19:13:46 +0300

    feature: local state status change

--- src/components/profile/profileInfo/ProfileStatus.jsx
+++ src/components/profile/profileInfo/ProfileStatus.jsx
@@ -3,17 +3,26 @@ import React from "react";
 class ProfileStatus extends React.Component {
     state = {
         editMode: false,
-        title: 'Yaas'
+        status: this.props.status
     }
-    activateEditMode ()  {
+
+    activateEditMode = () => {
         this.setState({
             editMode: true
-        })
+        });
     }
-    deactivateEditMode ()  {
+
+    deactivateEditMode = () => {
         this.setState({
             editMode: false
-        })
+        });
+        this.props.updateStatus(this.state.status);
+    }
+
+    onStatusChange = (e) => {
+        this.setState({
+            status: e.currentTarget.value
+        });
     }
 
     render() {
@@ -21,16 +30,22 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)}>{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode}>
+                            {this.props.status || "---"}</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input autoFocus={true} onBlur={this.deactivateEditMode.bind(this)} value={this.props.status}/>
+                        <input
+                            onChange={this.onStatusChange}
+                            autoFocus={true}
+                            onBlur={this.deactivateEditMode}
+                            value={this.state.status}
+                        />
                     </div>
                 }
             </div>
-        )
+        );
     }
 }
 
