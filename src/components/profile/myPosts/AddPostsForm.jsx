776f367ed9eea70bf4f83ace52f6296af8fc3093
Vika <kurilova2003@gmail.com>
2023-08-04 16:57:28 +0300

    feature: validations for posts

--- src/components/profile/myPosts/AddPostsForm.jsx
+++ src/components/profile/myPosts/AddPostsForm.jsx
@@ -1,17 +1,27 @@
 import {Field, Form, Formik} from "formik";
 import React from "react";
+import {Textarea} from "../../../common/formsControl/FormsControl";
 
 export const AddPostsForm = ({handleFormSubmit}) => {
-    const handleSubmit = (values, {resetForm}) => {
-        handleFormSubmit(values);
+    const handleSubmit = async (values, {resetForm}) => {
+        await handleFormSubmit(values);
         resetForm();
+    };
+    const validateForm = (values) => {
+        const errors = {};
+        if (!values.newPostText) {
+            errors.newPostText = "Field is required";
+        } else if (values.newPostText.length > 15) {
+            errors.newPostText = "Max length is 15 symbols";
+        }
+        return errors;
     };
 
     return (
-        <Formik initialValues={{newPostText: ''}} onSubmit={handleSubmit}>
+        <Formik initialValues={{newPostText: ''}} onSubmit={handleSubmit} validate={validateForm}>
             <Form>
                 <Field
-                    as="textarea"
+                    as={Textarea}
                     name="newPostText"
                     placeholder="Enter your post"
                 />
