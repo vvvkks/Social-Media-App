13272e2f965893f5eaa110ffc5955f49b3fb23e2
Vika <kurilova2003@gmail.com>
2023-08-11 17:15:08 +0300

    refactor: ProfileInfo component

--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -1,19 +1,16 @@
 import s from "./ProfileInfo.module.css"
 import Preloader from "../../../common/preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
-export const ProfileInfo = (props) => {
-	if (!props.profile) {
+export const ProfileInfo = ({profile, status, updateStatus}) => {
+	if (!profile) {
 		return <Preloader />
 	}
 	return (
 		<div>
-			{/*<div>*/}
-			{/*	<img src="https://wallpaperaccess.com/full/707529.jpg" className={s.profileBackground} alt="Content" />*/}
-			{/*</div>*/}
 			<div className={s.descriptionBlock}>
-				<img src={props.profile.photos.large} alt={'Content'}/>
-				<div>{props.profile.fullName}</div>
-				<ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+				<img src={profile.photos.large} alt={'Content'}/>
+				<div>{profile.fullName}</div>
+				<ProfileStatus status={status} updateStatus={updateStatus}/>
 			</div>
 		</div>
 	)
