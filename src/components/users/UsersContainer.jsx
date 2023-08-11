3b95d0e81b030447b8fe4e83aee22e31029554d6
Vika <kurilova2003@gmail.com>
2023-08-11 17:15:22 +0300

    refactor: UsersContainer component

--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -20,12 +20,14 @@ import {
 
 class UsersContainer extends React.Component {
     componentDidMount() {
-        this.props.requestUsers(this.props.currentPage, this.props.pageSize)
+        const {currentPage, pageSize} = this.props
+        this.props.requestUsers(currentPage, pageSize)
     }
 
     onPageChanged = (pageNumber) => {
+        const {pageSize} = this.props;
         this.props.setCurrentPage(pageNumber);
-        this.props.requestUsers(pageNumber, this.props.pageSize)
+        this.props.requestUsers(pageNumber, pageSize)
     }
 
     render() {
