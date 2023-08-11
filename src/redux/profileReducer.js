3d8dccac36aff2e1c701b9c03ae360c6d40a50f5
Vika <kurilova2003@gmail.com>
2023-08-11 15:00:45 +0300

    refactor: profileReducer

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -55,28 +55,22 @@ export const setUserProfile = (profile) => ({
 export const deletePost = (postId) => ({
     type: DELETE_POST, postId
 })
-export const getUserProfile = (userId) => (dispatch) => {
-    profileAPI.getProfileUserId(userId)
-        .then(data => {
-            dispatch(setUserProfile(data));
-        })
+export const getUserProfile = (userId) => async (dispatch) => {
+    let data = await profileAPI.getProfileUserId(userId)
+    dispatch(setUserProfile(data));
 }
 export const setStatus = (status) => ({
     type: SET_STATUS,
     status
 });
-export const getStatus = (userId) => (dispatch) => {
-    profileAPI.getStatus(userId)
-        .then(response => {
-            dispatch(setStatus(response.data));
-        });
+export const getStatus = (userId) => async (dispatch) => {
+    let response = await profileAPI.getStatus(userId)
+    dispatch(setStatus(response.data));
 }
-export const updateStatus = (status) => (dispatch) => {
-    profileAPI.updateStatus(status)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(setStatus(status));
-            }
-        })
+export const updateStatus = (status) => async (dispatch) => {
+    let response = await profileAPI.updateStatus(status)
+    if (response.data.resultCode === 0) {
+        dispatch(setStatus(status));
+    }
 }
 export default profileReducer;
