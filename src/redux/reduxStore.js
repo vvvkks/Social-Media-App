9bf3ba79b1d68d7de1892fcdbd850b7420d08791
Vika <kurilova2003@gmail.com>
2023-08-12 18:15:06 +0300

    add: chrome extensions

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,10 +1,10 @@
-import {applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux"
 import messageReducer from "./messageReducer";
 import profileReducer from "./profileReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from "redux-thunk"
 import appReducer from "./appReducer";
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -13,5 +13,8 @@ let reducers = combineReducers({
     auth: authReducer,
     app: appReducer
 });
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
+
+window.__store__ = store;
 export default store;
