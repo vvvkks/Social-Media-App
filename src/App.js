db6267806f748ea954c38ab5af5a417570301bb3
Vika <kurilova2003@gmail.com>
2023-08-13 19:03:45 +0300

    feature: Suspense for loading

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import HeaderContainer from "./components/header/HeaderContainer";
-import Navbar from "./components/navbar/Navbar";
+import React, { Suspense, lazy } from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import DialogsContainer from "./components/dialogs/DialogsContainer";
-import UsersContainer from "./components/users/UsersContainer";
-import ProfileContainer from "./components/profile/ProfileContainer";
-import React from "react";
-import LoginPage from "./components/login/Login";
 import {connect, Provider} from "react-redux";
 import { compose } from "redux";
 import { initializeApp } from "./redux/appReducer";
 import Preloader from "./common/preloader/Preloader";
 import store from "./redux/reduxStore";
+
+const HeaderContainer = lazy(() => import("./components/header/HeaderContainer"));
+const Navbar = lazy(() => import("./components/navbar/Navbar"));
+const DialogsContainer = lazy(() => import("./components/dialogs/DialogsContainer"));
+const UsersContainer = lazy(() => import("./components/users/UsersContainer"));
+const ProfileContainer = lazy(() => import("./components/profile/ProfileContainer"));
+const LoginPage = lazy(() => import("./components/login/Login"));
+
 
 class App extends React.Component {
     componentDidMount() {
@@ -23,16 +25,18 @@ class App extends React.Component {
         }
         return (
             <div className="app-wrapper">
-                <HeaderContainer />
-                <Navbar />
-                <div className="app-wrapper-content">
-                    <Routes>
-                        <Route exact path='/dialogs' element={<DialogsContainer />} />
-                        <Route exact path='/profile/:userId?' element={<ProfileContainer />} />
-                        <Route exact path='/users' element={<UsersContainer />} />
-                        <Route exact path='/login' element={<LoginPage />} />
-                    </Routes>
-                </div>
+                <Suspense fallback={<Preloader />}>
+                    <HeaderContainer />
+                    <Navbar />
+                    <div className="app-wrapper-content">
+                        <Routes>
+                            <Route exact path='/dialogs' element={<DialogsContainer />} />
+                            <Route exact path='/profile/:userId?' element={<ProfileContainer />} />
+                            <Route exact path='/users' element={<UsersContainer />} />
+                            <Route exact path='/login' element={<LoginPage />} />
+                        </Routes>
+                    </div>
+                </Suspense>
             </div>
         );
     }
@@ -52,5 +56,4 @@ const SocialApp = (props) => {
     </Router>
 }
 export default SocialApp;
-
 
