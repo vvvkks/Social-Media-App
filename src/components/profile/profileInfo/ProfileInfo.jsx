d0b5345657d5583af950b24f86405aaa1b94f385
Vika <kurilova2003@gmail.com>
2023-08-14 16:12:46 +0300

    add: profile information

--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -1,25 +1,52 @@
-import s from "./ProfileInfo.module.css"
+import React from "react";
+import s from "./ProfileInfo.module.css";
 import Preloader from "../../../common/preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
-import userPhoto from "../../../assets/images/user.png"
-export const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
+import userPhoto from "../../../assets/images/user.png";
+
+const Contact = ({ contactTitle, contactValue }) => {
+	return <div className={s.contact}><b>{contactTitle}</b>: {contactValue}</div>;
+};
+
+export const ProfileInfo = ({ profile, status, updateStatus, isOwner, savePhoto }) => {
 	if (!profile) {
-		return <Preloader />
+		return <Preloader />;
 	}
 
 	const onMainPhotoSelected = (e) => {
 		if (e.target.files.length) {
 			savePhoto(e.target.files[0]);
 		}
-	}
+	};
+
 	return (
 		<div>
 			<div className={s.descriptionBlock}>
-				<img src={profile.photos.large || userPhoto} alt={'Content'} className={s.mainPhoto}/>
-				{isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
-				<div>{profile.fullName}</div>
-				<ProfileStatus status={status} updateStatus={updateStatus}/>
+				<img src={profile.photos.large || userPhoto} alt={'Content'} className={s.mainPhoto} />
+				{isOwner && <input type={"file"} onChange={onMainPhotoSelected} />}
+				<div>
+					<div>
+						<b>Full name</b>: {profile.fullName}
+					</div>
+					<div>
+						<b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
+					</div>
+					{profile.lookingForAJob && (
+						<div>
+							<b>My skills</b>: {profile.lookingForAJobDescription}
+						</div>
+					)}
+					<div>
+						<b>About me</b>: {profile.aboutMe}
+					</div>
+					<div>
+						<b>Contacts</b>: {Object.keys(profile.contacts).map(key => (
+						<Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+					))}
+					</div>
+				</div>
+				<ProfileStatus status={status} updateStatus={updateStatus} />
 			</div>
 		</div>
-	)
-}
+	);
+};
--- src/components/profile/profileInfo/ProfileStatus.jsx
+++ src/components/profile/profileInfo/ProfileStatus.jsx
@@ -21,7 +21,7 @@ const ProfileStatus = (props) => {
         <div>
             {!editMode &&
                 <div>
-                    <span onDoubleClick={activateEditMode}>
+                    <b>Status: </b><span onDoubleClick={activateEditMode}>
                         {props.status || "---"}</span>
                 </div>
             }
