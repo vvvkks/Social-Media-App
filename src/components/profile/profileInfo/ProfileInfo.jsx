122da7f9d59d5571f96cf571a140f479680042c0
Vika <kurilova2003@gmail.com>
2023-08-14 14:18:03 +0300

    feature: upload photo for profile

--- src/components/profile/profileInfo/ProfileInfo.jsx
+++ src/components/profile/profileInfo/ProfileInfo.jsx
@@ -1,14 +1,22 @@
 import s from "./ProfileInfo.module.css"
 import Preloader from "../../../common/preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
-export const ProfileInfo = ({profile, status, updateStatus}) => {
+import userPhoto from "../../../assets/images/user.png"
+export const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
 	if (!profile) {
 		return <Preloader />
+	}
+
+	const onMainPhotoSelected = (e) => {
+		if (e.target.files.length) {
+			savePhoto(e.target.files[0]);
+		}
 	}
 	return (
 		<div>
 			<div className={s.descriptionBlock}>
-				<img src={profile.photos.large} alt={'Content'}/>
+				<img src={profile.photos.large || userPhoto} alt={'Content'} className={s.mainPhoto}/>
+				{isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
 				<div>{profile.fullName}</div>
 				<ProfileStatus status={status} updateStatus={updateStatus}/>
 			</div>
