f0f4c2e371221b0b6c0b1d575c14d244d21b6bc0
Vika <kurilova2003@gmail.com>
2023-09-06 22:12:07 +0300

    refactor: profileReducer

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -79,9 +79,13 @@ export const getStatus = (userId) => async (dispatch) => {
     dispatch(setStatus(response.data));
 }
 export const updateStatus = (status) => async (dispatch) => {
-    let response = await profileAPI.updateStatus(status)
-    if (response.data.resultCode === 0) {
-        dispatch(setStatus(status));
+    try {
+        let response = await profileAPI.updateStatus(status);
+        if (response.data.resultCode === 0) {
+            dispatch(setStatus(status));
+        }
+    } catch (error) {
+        //To-do: dispatch
     }
 }
 export const savePhoto = (file) => async (dispatch) => {
